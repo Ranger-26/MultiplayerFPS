888644498baca31f385a0f908442c90b218c894f
smohan67 <siddhu.mohan6@gmail.com>
2022-03-13 15:06:59 -0500

    improve spectator controller

--- Assets/Scripts/Game/Player/Spectating/SpectatorController.cs
+++ Assets/Scripts/Game/Player/Spectating/SpectatorController.cs
@@ -5,16 +5,6 @@ namespace Game.Player.Spectating
 {
     public class SpectatorController : NetworkBehaviour
     {
-        private CharacterController _controller;
-        private float _playerSpeed = 2.0f;
-
-
-        private void Start()
-        {
-            _controller = gameObject.GetComponent<CharacterController>();
-            _controller.detectCollisions = false;
-        }
-
         public override void OnStartLocalPlayer()
         {
             for(int i = 0; i < Camera.main.transform.childCount; i++)
@@ -24,25 +14,18 @@ namespace Game.Player.Spectating
             Camera.main.transform.SetParent(transform);
             Camera.main.transform.localPosition = new Vector3(0, 0, 0);
         }
+        
+        public float movementForwardMultiplier = 4f;
+        public float movementSideMultiplier = 4f;
 
-        private void Update()
-        {
-            if (hasAuthority)
-                CalculateMovement();
-        }
+        private string forwardAxis = "Vertical";
+        private string horizontalAxis = "Horizontal";
 
-        void CalculateMovement()
-        {
-            Vector3 move = new Vector3(UnityEngine.Input.GetAxis("Horizontal"), 0, UnityEngine.Input.GetAxis("Vertical"));
-            move = transform.transform.TransformDirection(move);
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                _controller.Move(move * Time.deltaTime * _playerSpeed * 2);
-            }
-            else
-            {
-                _controller.Move(move * Time.deltaTime * _playerSpeed);
-            }
+        void Update () {
+            float forwardMovement = Input.GetAxis(forwardAxis) * movementForwardMultiplier * Time.deltaTime;
+            float horizontalMovement = Input.GetAxis(horizontalAxis) * movementSideMultiplier * Time.deltaTime;
+            Vector3 movementDelta = new Vector3(horizontalMovement, 0, forwardMovement);
+            transform.position += transform.TransformDirection(movementDelta);
         }
-    }
+    }   
 }

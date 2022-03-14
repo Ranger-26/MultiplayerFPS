7dd2ec25335bb3b539b1d194f15fd24234d5d36e
smohan67 <siddhu.mohan6@gmail.com>
2022-03-13 20:53:15 -0500

    quick refactoring

--- Assets/Scripts/Game/Player/Throwables/ThrowableScriptable.cs
+++ Assets/Scripts/Game/Player/Throwables/ThrowableScriptable.cs
@@ -1,20 +1,23 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Throwable", menuName = "Throwable")]
-public class ThrowableScriptable : ScriptableObject
+namespace Game.Player.Throwables
 {
-    public GameObject ThrowObject;
-
-    public float Velocity;
-    public float Timer;
-
-    public virtual void OnCollide(Collision coll, GameObject obj)
+    [CreateAssetMenu(fileName = "New Throwable", menuName = "Throwable")]
+    public class ThrowableScriptable : ScriptableObject
     {
+        public GameObject ThrowObject;
 
-    }
+        public float Velocity;
+        public float Timer;
 
-    public virtual void Trigger(Transform location)
-    {
+        public virtual void OnCollide(Collision coll, GameObject obj)
+        {
 
+        }
+
+        public virtual void Trigger(Transform location)
+        {
+
+        }
     }
 }

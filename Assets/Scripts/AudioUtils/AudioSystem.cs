226fd778e5c07f4352d1140b6f46900b884c4b08
smohan67 <siddhu.mohan6@gmail.com>
2022-04-10 18:13:33 -0500

    remove unecessary overload

--- Assets/Scripts/AudioUtils/AudioSystem.cs
+++ Assets/Scripts/AudioUtils/AudioSystem.cs
@@ -25,26 +25,6 @@ namespace AudioUtils
             au.Play();
         }
 
-        public static void PlaySound(this AudioClip _sound, Transform _parent, float _maxDistance, float _volume, float _pitch, float _spatialBlend, int _priority)
-        {
-            GameObject soundObj = new GameObject("Sound", typeof(AudioSource), typeof(DestroyAfter));
-            AudioSource au = soundObj.GetComponent<AudioSource>();
-            DestroyAfter des = soundObj.GetComponent<DestroyAfter>();
-            soundObj.transform.parent = _parent;
-            soundObj.transform.localPosition = Vector3.zero;
-            au.playOnAwake = false;
-            au.clip = _sound;
-            au.maxDistance = _maxDistance;
-            au.volume = _volume;
-            au.pitch = _pitch;
-            au.spatialBlend = _spatialBlend;
-            au.priority = _priority;
-            des.Timer = _sound.length - 0.1f;
-            au.rolloffMode = AudioRolloffMode.Linear;
-            au.minDistance = 1.5f;
-            au.Play();
-        }
-
         public static void NetworkPlaySound(this AudioClip _sound, Vector3 _position, float _maxDistance, float _volume, float _pitch, float _spatialBlend, int _priority)
         {
             AudioMessage message = new AudioMessage()
@@ -57,7 +37,7 @@ namespace AudioUtils
                 spatialBlend = _spatialBlend,
                 priority = _priority
             };
-            NetworkClient.Send(message);
+            NetworkClient.Send(message, Channels.Unreliable);
         }
         
         public static void OnClientReceiveAudioMessage(AudioMessage message)

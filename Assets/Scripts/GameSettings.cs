1755d94d9a98acac5f941b8b3bfc94e57c8122af
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2022-05-27 08:05:00 -0500

    Update GameSettings.cs

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -4,12 +4,12 @@ public static class GameSettingsLoader
 {
     public static void SaveFile(Settings sett)
     {
-        ES3.Save("settings", sett, Application.persistentDataPath + "/Eternity Studios/SCP_Intrusion/Settings");
+        ES3.Save("settings", sett, Application.persistentDataPath + "/Eternity Studios/SCP Intrusion/Settings");
     }
 
     public static Settings LoadFile()
     {
-        return ES3.Load<Settings>("settings", Application.persistentDataPath + "/Eternity Studios/SCP_Intrusion/Settings");
+        return ES3.Load<Settings>("settings", Application.persistentDataPath + "/Eternity Studios/SCP Intrusion/Settings");
     }
 }
 

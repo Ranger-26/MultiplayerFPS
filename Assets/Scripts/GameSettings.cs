a10af3a9905a46d2f598d00ca0b1b139fac2626a
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2022-05-28 08:46:20 -0500

    crosshair no work

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -23,5 +23,33 @@ public class Settings
 {
     public float Sensitivity;
 
-    public Crosshair ch;
+    public CrosshairSettings ch = new CrosshairSettings();
+}
+
+[System.Serializable]
+public class CrosshairSettings
+{
+    public int offset = 5;
+    public int length = 10;
+    public int thickness = 3;
+
+    public Color color = new Color(255f, 255f, 255f, 100f);
+
+    public float firingErrorMultiplier = 10f;
+
+    public bool firingError = true;
+
+    public CrosshairSettings()
+    {
+        offset = 5;
+        length = 10;
+        thickness = 3;
+    }
+
+    public CrosshairSettings(int _thickness, int _length, int _offset)
+    {
+        offset = _offset;
+        length = _length;
+        thickness = _thickness;
+    }
 }
--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -6,15 +6,39 @@ public class SettingsMenu : MonoBehaviour
     [SerializeField]
     InputField sens;
 
+    [SerializeField]
+    InputField c_width;
+    [SerializeField]
+    InputField c_height;
+    [SerializeField]
+    InputField c_offset;
+
     private void OnEnable()
     {
         sens.text = GameSettings.current.Sensitivity.ToString();
+
+        Vector3 crs = new Vector3(GameSettings.current.ch.length, GameSettings.current.ch.thickness, GameSettings.current.ch.offset);
+
+        c_width.text = ((int)crs.x).ToString();
+        c_height.text = ((int)crs.y).ToString();
+        c_offset.text = ((int)crs.z).ToString();
     }
 
     public void Sensitivity()
     {
         float sen = float.Parse(sens.text);
         GameSettings.current.Sensitivity = sen;
+        GameSettingsLoader.SaveFile(GameSettings.current);
+    }
+
+    public void SetCrosshair()
+    {
+        Vector3 crs = new Vector3(int.Parse(c_width.text), int.Parse(c_height.text), int.Parse(c_offset.text));
+
+        GameSettings.current.ch.length = (int)crs.x;
+        GameSettings.current.ch.thickness = (int)crs.y;
+        GameSettings.current.ch.offset = (int)crs.z;
+
         GameSettingsLoader.SaveFile(GameSettings.current);
     }
 }

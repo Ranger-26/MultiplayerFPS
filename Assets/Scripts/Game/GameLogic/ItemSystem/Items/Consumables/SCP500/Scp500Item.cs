250d62152e486d7fddb3ac863f53a8428f78fba5
smohan67 <siddhu.mohan6@gmail.com>
2022-08-02 18:10:59 -0500

    scp500 item works

--- Assets/Scripts/Game/GameLogic/ItemSystem/Items/Consumables/SCP500/Scp500Item.cs
+++ Assets/Scripts/Game/GameLogic/ItemSystem/Items/Consumables/SCP500/Scp500Item.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using Game.GameLogic.ItemSystem.Core;
 using Game.Player;
+using Inputs;
 using Mirror;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -11,6 +12,8 @@ namespace Game.GameLogic.ItemSystem.Items.Consumables.SCP500
     {
         public Animator Animator;
 
+        public bool InUse;
+        
         public override void InitItem(NetworkGamePlayer owner)
         {
             base.InitItem(owner);
@@ -19,18 +22,38 @@ namespace Game.GameLogic.ItemSystem.Items.Consumables.SCP500
 
         public override void OnUse(InputAction.CallbackContext ctx)
         {
-            NetworkClient.Send(new Scp500Message()
-            {
-                
-            });
-            StartCoroutine(TestAnim());
+            StartCoroutine(Test());
+            InUse = true;
         }
 
-        public IEnumerator TestAnim()
+        public IEnumerator Test()
         {
             transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
             yield return new WaitForSeconds(3f);
             transform.Rotate(-90.0f, 0.0f, 0.0f, Space.Self);
+            NetworkClient.Send(new Scp500Message());
+        }
+
+        public void CancelUse(InputAction.CallbackContext ctx)
+        {
+            StopCoroutine(Test());
+        }
+
+        public override void ResetViewModel()
+        {
+            transform.localPosition = new Vector3(0.22f, 0, 2.32f);
+        }
+
+        protected override void RegisterInputEvents()
+        {
+            base.RegisterInputEvents();
+            GameInputManager.Actions.Player.AltFire.performed += CancelUse;
+        }
+
+        protected override void UnRegisterInputEvents()
+        {
+            base.UnRegisterInputEvents();
+            GameInputManager.Actions.Player.AltFire.performed -= CancelUse;
         }
     }
 }

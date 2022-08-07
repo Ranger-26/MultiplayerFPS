97cabdb30d5e46af9fc9a695b36f5a1ebf558f95
smohan67 <siddhu.mohan6@gmail.com>
2022-08-07 14:28:42 -0500

    reimplement scp500 to make creating new consumables easier

---
+++ Assets/Scripts/Game/GameLogic/ItemSystem/Items/Consumables/GenericConsumables/ConsumableBase.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using Game.GameLogic.ItemSystem.Core;
+using Game.GameLogic.ItemSystem.Inventory;
+using Game.Player;
+using Inputs;
+using Mirror;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Game.GameLogic.ItemSystem.Items.Consumables.GenericConsumables
+{
+    public class ConsumableBase : ItemBase, IConsumable
+    {
+        [HideInInspector]
+        public ConsumableData ConsumableData;
+
+        public override void InitItem(NetworkGamePlayer owner)
+        {
+            base.InitItem(owner);
+            ConsumableData = (ConsumableData) ItemData;
+        }
+
+        public override bool OnEquip()
+        {
+            SubscribeToInputEvents();
+            return true;
+        }
+
+        public override bool OnDeEquip()
+        {
+            UnSubscribeFromInputEvents();
+            return true;
+        }
+
+        public virtual void SubscribeToInputEvents()
+        {
+            GameInputManager.Actions.Player.Fire.performed += OnClientUse;
+        }
+
+        public virtual void UnSubscribeFromInputEvents()
+        {
+            GameInputManager.Actions.Player.Fire.performed -= OnClientUse;
+        }
+        
+        
+        public virtual void ServerOnConsume()
+        {
+            Owner.GetComponent<PlayerInventory>().ServerDestroyHeldItem();
+        }
+
+        public virtual void OnClientUse(InputAction.CallbackContext ctx)
+        {
+            NetworkClient.Send(new GenericConsumableMessage());
+        }
+    }
+}

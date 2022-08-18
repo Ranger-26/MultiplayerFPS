dfa5e002f6c0cd2b56a412f470305a4319340548
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2022-08-18 16:59:37 -0400

    Update MaterialModifierScriptable.cs

--- Assets/Scripts/Game/GameLogic/ItemSystem/Items/Firearms/Gunplay/MaterialModifierScriptable.cs
+++ Assets/Scripts/Game/GameLogic/ItemSystem/Items/Firearms/Gunplay/MaterialModifierScriptable.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Material Modifier", menuName = "Material Modifier")]
 public class MaterialModifierScriptable : ScriptableObject
 {
-    public GameObject DecalOverride;
+    public GameObject DecalOverride; // Handle this sometime, idk, different bullet holes on different materials
 
     public float WallbangMultiplier = 1f;
 }

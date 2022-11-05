1fe8e68bb269709282ee6a357d11f8dbaef25a14
smohan67 <siddhu.mohan6@gmail.com>
2022-11-04 19:54:33 -0500

    docs

--- Assets/Scripts/Game/GameLogic/ItemSystem/Core/UsableItems/UsableItemBase.cs
+++ Assets/Scripts/Game/GameLogic/ItemSystem/Core/UsableItems/UsableItemBase.cs
@@ -7,12 +7,19 @@ namespace Game.GameLogic.ItemSystem.Core.UsableItems
 {
     public abstract class UsableItemBase : ItemBase
     {
+        /// <summary>
+        /// Called when the client wants to use the item.
+        /// </summary>
+        /// <param name="ctx">Input Context</param>
         public virtual void OnClientUse(InputAction.CallbackContext ctx)
         {
             //client stuff
             SendUseMessage();
         }
 
+        /// <summary>
+        /// Tells tell server that the client wants to use the item.
+        /// </summary>
         protected void SendUseMessage()
         {
             NetworkClient.Send(new UseUsableItemMessage()
@@ -21,6 +28,9 @@ namespace Game.GameLogic.ItemSystem.Core.UsableItems
             });
         }
 
+        /// <summary>
+        /// Called on the server when it receives a message to use an item.
+        /// </summary>
         public virtual void OnServerReceiveUseMessage()
         {
             
